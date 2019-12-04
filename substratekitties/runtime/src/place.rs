661b9a47dd6bdebc9e5a3208a14185e0575d9e46
Julian Konchunas <konchunas@gmail.com>
2019-12-04 18:01:03 +0200

    Switch to coordinate addressing for chunks

--- substratekitties/runtime/src/place.rs
+++ substratekitties/runtime/src/place.rs
@@ -1,9 +1,9 @@
 use rstd::prelude::*;
+
+use runtime_primitives::traits::As;
 use runtime_io::print;
-use runtime_primitives::traits::{As, Hash, Zero};
-use support::storage_items;
 use support::{
-    decl_event, decl_module, decl_storage, dispatch::Result, ensure, traits::Currency, StorageMap,
+    decl_event, decl_module, decl_storage, dispatch::Result, StorageMap,
     StorageValue,
 };
 use system::ensure_signed;
@@ -12,7 +12,6 @@ use parity_codec::{Decode, Encode};
 // use runtime_io::with_storage;
 
 const CHUNK_SIDE: usize = 8;
-const FIELD_SIDE: usize = 8;
 
 type Color = [u8; 3];
 
@@ -33,29 +32,20 @@ decl_event!(
         <T as system::Trait>::AccountId,
         <T as balances::Trait>::Balance
     {
-        Bought(AccountId, u16, u16, Balance),
+        Bought(AccountId, i64, i64, Balance),
     }
 );
 
 decl_storage! {
     trait Store for Module<T: Trait> as PlaceStorage {
-
-        OwnedPixelArray get(pixel_of_owner_by_index): map (T::AccountId, u64) => (u16, u16);
+        /// whole field is stored as 8x8 chunks and chunks are stored in i32xi32 map
+        /// to store absolute pixel coordinates we would need i36 since there is 8 more coordinates per chunk
+        /// but i64 is the closest we have to i36
+        OwnedPixelArray get(pixel_of_owner_by_index): map (T::AccountId, u64) => (i64, i64);
         OwnedPixelCount get(owned_pixel_count): map T::AccountId => u64;
-        PixelOwner get(owner_of): map (u16, u16) => Option<T::AccountId>;
+        PixelOwner get(owner_of): map (i64, i64) => Option<T::AccountId>;
         
-        Chunks get(chunks) build(|_config: &GenesisConfig<T>| {
-            let empty = Pixel {
-                price: <T::Balance as As<u64>>::sa(1),
-                color: [0,0,0]
-            };
-			(0..FIELD_SIDE)
-				.map(|i| (
-					i as u16,
-					vec![empty.clone(); CHUNK_SIDE*CHUNK_SIDE]
-				))
-				.collect::<Vec<_>>()
-		}): map u16 => Vec<Pixel<T::Balance>>;
+        Chunks get(chunks): map (i32, i32) => Vec<Pixel<T::Balance>>;
 
     }
 }
@@ -65,16 +55,21 @@ decl_module! {
 
         fn deposit_event<T>() = default;
 
-        fn purchase_pixel(origin, x: u16, y: u16, color: Color, new_price: T::Balance) -> Result {
+        fn purchase_pixel(origin, x: i64, y: i64, color: Color, new_price: T::Balance) -> Result {
             let sender = ensure_signed(origin)?;
 
-            let (chunk_num, index) = from_cartesian(x,y);
-            print(chunk_num as u64);
+            print("Before from absolute");
+            let ((chunk_x, chunk_y), index) = from_absolute(x,y);
+            print("From absolute");
 
-            if !<Chunks<T>>::exists(chunk_num) {
-                Self::init_chunk(chunk_num)?;
+
+            if !<Chunks<T>>::exists((chunk_x, chunk_y)) {
+                Self::init_chunk(chunk_x, chunk_y)?;
             }
 
+            print("Index");
+            print(index as u64);
+            
             let owned_pixel_count = Self::owned_pixel_count(&sender);
             let new_owned_pixel_count = owned_pixel_count.checked_add(1).ok_or("Overflow buying new pixel for user")?;
 
@@ -87,8 +82,10 @@ decl_module! {
                 price: new_price,
                 color
             };
+            print("new pixel");
+            <Chunks<T>>::mutate((chunk_x, chunk_y), |chunk| chunk[index as usize] = new_pixel);
+            print("mutation complete");
 
-            <Chunks<T>>::mutate(chunk_num, |chunk| chunk[index as usize] = new_pixel);
 
             Self::deposit_event(RawEvent::Bought(sender, x, y, new_price));
 
@@ -97,20 +94,18 @@ decl_module! {
 
         /// Initialize method for root to create empty field of specified size 
         /// TODO remove origin
-        pub fn initialize_field(origin, field_size: u16) -> Result {
+        pub fn initialize_field(origin, field_size: i32) -> Result {
             let empty = Pixel {
                 price: <T::Balance as As<u64>>::sa(1),
                 color: [255,255,255]
             };
-            for i in 0..field_size*field_size {
-                let chunk = vec![empty.clone(); CHUNK_SIDE*CHUNK_SIDE];
-                <Chunks<T>>::insert(i, chunk);
+            for i in 0..field_size {
+                for j in 0..field_size {
+                    let chunk = vec![empty.clone(); CHUNK_SIDE*CHUNK_SIDE];
+                    <Chunks<T>>::insert((i,j), chunk);
+                }
             }
             Ok(())
-        }
-
-        fn create_fixed_pixel(origin,  x: u16, y: u16, price: T::Balance) -> Result {
-            Self::purchase_pixel(origin, x, y, [1,2,3], <T::Balance as As<u64>>::sa(1))
         }
 
         // fn obtain_region(x1: u16, y1: u16, x2: u16, y2: u16) -> Result<Color, &'static str> {
@@ -121,58 +116,144 @@ decl_module! {
 }
 
 impl<T: Trait> Module<T> {
-    fn init_chunk(index: u16) -> Result {
+    fn init_chunk(x: i32, y: i32) -> Result {
         let empty_pixel = Pixel {
             price: <T::Balance as As<u64>>::sa(1), //minimum price
             color: [255, 255, 255], //white
         };
-        <Chunks<T>>::insert(index, vec![empty_pixel.clone(); CHUNK_SIDE * CHUNK_SIDE]);
+        <Chunks<T>>::insert((x,y), vec![empty_pixel.clone(); CHUNK_SIDE * CHUNK_SIDE]);
         Ok(())
-}
-}
-
-fn from_cartesian(x: u16, y: u16) -> (u16, u16) {
-    let chunk_x = x / 8;
-    let chunk_y = y / 8;
-    let chunk_number = chunk_x + chunk_y * FIELD_SIDE as u16;
-    
-    let local_x = x % 8;
-    let local_y = y % 8;
-    let index = local_x + local_y * CHUNK_SIDE as u16;
-
-    (chunk_number, index)
+    }
 }
 
-fn to_cartesian(x: u16, y: u16) -> (u16, u16) {
-    (2, 3)
+fn from_absolute(x: i64, y: i64) -> ((i32, i32), u8) {
+    let (chunk_x, local_x) = convert_coord(x);
+    let (chunk_y, local_y) = convert_coord(y);
+
+    let index = local_x as u8 + local_y as u8 * CHUNK_SIDE as u8;
+
+    ((chunk_x, chunk_y), index)
+}
+
+fn convert_coord(c: i64) -> (i32, u8) {
+    let chunk = c.div_euclid(CHUNK_SIDE as i64);
+    let local = c.rem_euclid(CHUNK_SIDE as i64);
+    (chunk as i32, local as u8)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // Import a bunch of dependencies from substrate core. All needed for some parts of the code.
+    use support::{impl_outer_origin, assert_ok};
+    use runtime_io::{with_externalities, TestExternalities};
+    use primitives::{H256, Blake2Hasher};
+    use runtime_primitives::{
+        BuildStorage,
+        traits::{BlakeTwo256, IdentityLookup},
+        testing::{Digest, DigestItem, Header}
+    };
+
+    impl_outer_origin! {
+        pub enum Origin for PlaceTest {}
+    }
+
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct PlaceTest;
+
+    impl system::Trait for PlaceTest {
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type Digest = Digest;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type Log = DigestItem;
+    }
+
+    impl balances::Trait for PlaceTest {
+        type Balance = u64;
+        type OnFreeBalanceZero = ();
+        type OnNewAccount = ();
+        type Event = ();
+        type TransactionPayment = ();
+        type TransferPayment = ();
+        type DustRemoval = ();
+    }
+
+    impl super::Trait for PlaceTest {
+        type Event = ();
+    }
+
+    type Place = super::Module<PlaceTest>;
+
+    fn build_ext() -> TestExternalities<Blake2Hasher> {
+        let mut t = system::GenesisConfig::<PlaceTest>::default().build_storage().unwrap().0;
+        t.extend(balances::GenesisConfig::<PlaceTest>::default().build_storage().unwrap().0);
+        t.into()
+    }
+
+    #[test]
+    fn purchase_pixel_should_work() {
+        with_externalities(&mut build_ext(), || {
+            // let prive = <PlaceTest::Balance as As<u64>>::sa(3);
+            assert_ok!(Place::purchase_pixel(Origin::signed(10), 0, 0, [1,2,3], 3));
+
+            let chunk = Place::chunks((0, 0));
+            assert_eq!(chunk[0].price, 3);
+        })
+    }
+
+    #[test]
+    fn purchase_negative_chunk_should_work() {
+        with_externalities(&mut build_ext(), || {
+            // let prive = <PlaceTest::Balance as As<u64>>::sa(3);
+            assert_ok!(Place::purchase_pixel(Origin::signed(10), -1, -1, [1,2,3], 3));
+
+            let chunk = Place::chunks((-1, -1));
+            assert_eq!(chunk[63].price, 3);
+        })
+    }
+
+    #[test]
+    fn init_negative_chunk_should_work() {
+        with_externalities(&mut build_ext(), || {
+            // let prive = <PlaceTest::Balance as As<u64>>::sa(3);
+            assert_ok!(Place::init_chunk(-1,-1));
+
+            let chunk = Place::chunks((-1, -1));
+            assert!(!chunk.is_empty());
+
+            assert_eq!(chunk[0].price, 1);
+        })
+    }
+
+    #[test]
+    fn pixel_convertsion_from_absolute() {
+        assert_eq!(from_absolute(0, 0), ((0, 0), 0));
+        assert_eq!(from_absolute(1, 0), ((0, 0), 1));
+        assert_eq!(from_absolute(0, 1), ((0, 0), 8));
+        assert_eq!(from_absolute(1, 1), ((0, 0), 9));
+        assert_eq!(from_absolute(0, 2), ((0, 0), 16));
+    }
     
     #[test]
-    fn cartesian_conversion_first_chunk() {
-        assert_eq!(from_cartesian(0, 0), (0, 0));
-        assert_eq!(from_cartesian(7, 0), (0, 7));
-        assert_eq!(from_cartesian(0, 1), (0, 8));
-        assert_eq!(from_cartesian(1, 1), (0, 9));
-        assert_eq!(from_cartesian(0, 2), (0, 16));
+    fn chunk_convertsion_from_absolute() {
+        assert_eq!(from_absolute(0, 0), ((0, 0), 0));
+        assert_eq!(from_absolute(8, 0), ((1, 0), 0));
+        assert_eq!(from_absolute(0, 8), ((0, 1), 0));
     }
 
     #[test]
-    fn cartesian_conversion_chunk_numbers() {
-        assert_eq!(from_cartesian(8, 0), (1, 0));
-        assert_eq!(from_cartesian(9, 0), (1, 1));
-        assert_eq!(from_cartesian(8, 1), (1, 8));
-        assert_eq!(from_cartesian(15, 0), (1, 7));
-
-        assert_eq!(from_cartesian(0, 8), (8, 0));
-        assert_eq!(from_cartesian(0, 9), (8, 8));
-
-        assert_eq!(from_cartesian(8, 8), (9, 0));
-        assert_eq!(from_cartesian(16, 16), (18, 0));
-
-        assert_eq!(from_cartesian(56, 0), (7, 0));
+    fn negative_convertsion_from_absolute() {
+        assert_eq!(from_absolute(-1, -1), ((-1, -1), 63));
+        assert_eq!(from_absolute(-8, -8), ((-1, -1), 0));
+        assert_eq!(from_absolute(-9, -9), ((-2, -2), 63));
     }
-    }
+    
+}
