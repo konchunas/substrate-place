93dff7e2a2820be08ea2d46a453ef7f76926b273
Julian Konchunas <konchunas@gmail.com>
2019-12-19 00:19:12 +0200

    Remove old names and ui package

--- substratekitties-ui/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const port = 1488;
-let app = express();
-app.use(express.static('dist'));
-
-app.listen(port, () => {
-  console.log('Listening on port 8000');
-});
